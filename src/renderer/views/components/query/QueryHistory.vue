58f9df48acb2d0f9266ac22019fde3fc4da52c41
qianmoQ <shicheng@ttxit.com>
2021-06-30 21:50:09 +0800

    Refactoring query history UI

--- src/renderer/views/components/query/QueryHistory.vue
+++ src/renderer/views/components/query/QueryHistory.vue
@@ -9,7 +9,27 @@
         <el-button type="danger" icon="el-icon-delete" size="mini" @click="handlerClearHistory()"></el-button>
       </el-tooltip>
     </el-row>
-    <table-detail :columns="data.columns" :headers="data.headers" :loading="tableBodyLoading"></table-detail>
+
+    <el-table v-loading.body="tableBodyLoading"
+      style="width: 100%"
+      :data="data.columns">
+      <el-table-column>
+        <template slot-scope="scope">
+          <el-card :class="'box-card ' + (scope.row.status ? 'success' : 'error')">
+            <div slot="header" class="clearfix">
+              <i
+                :class="'fa fa-' + (scope.row.status ? 'check-circle' : 'exclamation-circle')"
+                :style="'color: ' + (scope.row.status ? '#67C23A' : '#F56C6C') + ';'">
+              </i>
+              <span>{{ scope.row.startTime }}</span>
+            </div>
+            {{ scope.row.query }}
+            <p v-if="!scope.row.status"> {{ scope.row.message }}</p>
+          </el-card>
+        </template>
+      </el-table-column>
+    </el-table>
+
     <div slot="footer" class="dialog-footer">
       <el-button @click="bodyLoading = false" size="mini">{{ this.$t('common.cancel') }}</el-button>
     </div>
@@ -76,3 +96,22 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  /deep/ .el-card {
+    color: #FFFFFF;
+  }
+  /deep/ .el-card__header {
+    padding: 5px 15px;
+    border-bottom: 0px solid #ebeef5;
+  }
+  /deep/ .el-card__body {
+    padding: 5px 15px;
+  }
+  .success {
+    background-color: #557844;
+  }
+  .error {
+    background-color: #bd8c8c;
+  }
+</style>

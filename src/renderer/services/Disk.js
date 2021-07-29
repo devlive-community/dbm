07b8a74d564e3785a75f05b7122b958c93ac0e65
qianmoQ <shicheng@ttxit.com>
2021-07-29 21:30:04 +0800

    [Enhancement] Support service nodes to display database disk usage

--- src/renderer/services/Disk.js
+++ src/renderer/services/Disk.js
@@ -1,5 +1,10 @@
 import { getQuery } from '@/services/Metadata'
 
+/**
+ * Get server used disk
+ * @param server remote server
+ * @returns {Promise<*>}
+ */
 export async function getDisks(server) {
   const sql = `
 SELECT
@@ -16,6 +21,37 @@ SELECT
   round((total_space - free_space) / total_space * 100, 3) AS value
 FROM
   system.disks
+  `
+  return await getQuery(server, sql)
+}
+
+/**
+ * Get database used disk from server
+ * @param server remote server
+ * @returns {Promise<*>}
+ */
+export async function getDisksByDb(server) {
+  const sql = `
+WITH t0 AS (
+  SELECT
+    total_space AS totalBytes,
+    total_space - free_space AS usedBytes
+  FROM system.disks
+),
+t1 AS (
+  SELECT 
+    database, 
+    SUM(bytes_on_disk) AS dbUsedBytes
+  FROM system.parts
+  GROUP BY database
+)
+SELECT
+  t1.database AS name,
+  formatReadableSize(t0.totalBytes) AS totalSize,
+  formatReadableSize(t0.usedBytes) AS usedSize,
+  formatReadableSize(t1.dbUsedBytes) AS dbUsedSize,
+  round(t1.dbUsedBytes / t0.totalBytes * 100, 5) AS value
+FROM t0, t1
   `
   return await getQuery(server, sql)
 }

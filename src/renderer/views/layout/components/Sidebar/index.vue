023865b170ebc91287771d31f09c87a3d8e653ba
qianmoQ <shicheng@ttxit.com>
2021-08-15 21:44:58 +0800

    [Docs] Add 1.4.0 release note

--- src/renderer/views/layout/components/Sidebar/index.vue
+++ src/renderer/views/layout/components/Sidebar/index.vue
@@ -32,7 +32,7 @@
               <el-menu-item :index="item.path+'/'+child.path">
                 <i v-if="child.meta&&child.meta.icon" :class="'fa fa-' + child.meta.icon"></i>
                 <span v-if="child.meta&&child.meta.title" slot="title">
-                  {{child.meta.title}} 
+                  {{child.meta.title}}
                   <el-tag v-if="item.children[0].new" type="danger" size="mini" effect="dark">{{ $t('common.new') }}</el-tag>
                 </span>
               </el-menu-item>
@@ -43,7 +43,7 @@
       <el-submenu :index="'rightInfo'" style="float: right;">
         <template slot="title">
           <div class="avatar-wrapper">
-            <span>DBM(Incubator)</span>
+            <span>DBM</span>
             <i class="el-icon-caret-bottom"></i>
           </div>
         </template>

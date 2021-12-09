d666fa6915e4c939cf5a690e3f287ec891f3ce2f
qianmoQ <shicheng@ttxit.com>
2021-12-09 12:42:37 +0800

    [Refactor] Replace the template for query

--- src/renderer/app/pages/query/query.component.ts
+++ src/renderer/app/pages/query/query.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { BaseComponent } from '@renderer/app/base.component';
 import { EditorService } from '@renderer/services/editor/editor.service';
 import { DatasourceService } from '@renderer/services/management/datasource.service';
@@ -6,34 +6,43 @@ import { DatasourceModel } from '@renderer/model/datasource.model';
 import { QueryService } from '@renderer/services/query/query.service';
 import { RequestModel } from '@renderer/model/request.model';
 import { StringUtils } from '@renderer/utils/string.utils';
-import { SqlUtils } from '@renderer/utils/sql.utils';
 import { QueryHistoryModel } from '@renderer/model/query.history.model';
 import { Md5 } from 'ts-md5';
 import { QueryHistoryService } from '@renderer/services/query/query.history.service';
+import { NzMessageService } from 'ng-zorro-antd/message';
 import { StateEnum } from '@renderer/enum/state.enum';
+import { SqlUtils } from '@renderer/utils/sql.utils';
+import { ResponseDataModel } from '@renderer/model/response.model';
 
 @Component({
   selector: 'app-query',
   templateUrl: 'query.component.html'
 })
 export class QueryComponent extends BaseComponent {
-  @ViewChild('codeEditor')
-  private codeEditor;
+  @ViewChildren('codeEditors')
+  private codeEditors: QueryList<ElementRef>;
   editorConfig: any;
-  datasources: DatasourceModel[];
+  dataSources: DatasourceModel[];
   datasource: string;
   disabledButton = {
     execute: true
   };
-  responseTableData: any;
+  responseTableData: ResponseDataModel[] = new Array();
+  editorContainers = [];
+  resultContainers = [];
+  containerSelected = 0;
 
   constructor(private editorService: EditorService,
               private datasourceService: DatasourceService,
               private queryService: QueryService,
+              private messageService: NzMessageService,
               private queryHistoryService: QueryHistoryService) {
     super();
     this.editorConfig = this.editorService.getDefault();
-    this.datasources = this.datasourceService.getAll()?.data?.columns;
+    this.dataSources = this.datasourceService.getAll()?.data?.columns;
+    this.editorContainers.push('Editor ' + 1);
+    this.resultContainers.push('Editor ' + 1 + ' Result');
+    this.responseTableData.push(new ResponseDataModel());
   }
 
   handlerCheckStatus() {
@@ -48,16 +57,15 @@ export class QueryComponent extends BaseComponent {
     queryHistory.query = sql;
     queryHistory.startTime = Date.parse(new Date().toString());
     const request = new RequestModel();
-    this.responseTableData = null;
     request.config = this.datasourceService.getAll(this.datasource)?.data?.columns[0];
     queryHistory.server = this.datasource;
-    sql = StringUtils.isEmpty(sql) ? this.codeEditor.codeMirror.getValue() : sql;
+    sql = StringUtils.isEmpty(sql) ? this.codeEditors.get(this.containerSelected)['codeMirror'].getValue() : sql;
     this.queryService.getResponse(request, sql).then(response => {
       if (response.status) {
-        this.responseTableData = response.data;
+        this.responseTableData[this.containerSelected] = response.data;
         queryHistory.state = StateEnum.success;
       } else {
-        // this.toastrService.error(response.message);
+        this.messageService.error(response.message);
         queryHistory.message = response.message;
         queryHistory.state = StateEnum.failure;
       }
@@ -68,10 +76,24 @@ export class QueryComponent extends BaseComponent {
   }
 
   handlerSelectionExecute() {
-    this.handlerExecute(this.codeEditor.codeMirror.getSelection());
+    const codeMirror = this.codeEditors.get(this.containerSelected)['codeMirror'];
+    this.handlerExecute(codeMirror.getSelection());
   }
 
   handlerFormatter() {
-    this.codeEditor.codeMirror.setValue(SqlUtils.formatter(this.codeEditor.codeMirror.getValue()));
+    const codeMirror = this.codeEditors.get(this.containerSelected)['codeMirror'];
+    codeMirror.setValue(SqlUtils.formatter(codeMirror.getValue()));
+  }
+
+  handlerAddContainer() {
+    this.editorContainers.push('Editor ' + (this.editorContainers.length + 1));
+    this.containerSelected = this.editorContainers.length;
+    this.resultContainers.push('Editor ' + this.containerSelected + ' Result');
+    this.responseTableData.push(new ResponseDataModel());
+  }
+
+  handlerCloseContainer({index}: { index: number }) {
+    this.editorContainers.splice(index, 1);
+    this.resultContainers.splice(index, 1);
   }
 }

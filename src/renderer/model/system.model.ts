56bfad1181a5cf3ebd7a63035f07c6894111d816
qianmoQ <shicheng@ttxit.com>
2021-12-17 00:34:51 +0800

    [Refactor] Add format to basic setting

--- src/renderer/model/system.model.ts
+++ src/renderer/model/system.model.ts
@@ -1,5 +1,7 @@
 import { BaseModel } from '@renderer/model/base.model';
+import { FormatEnum } from '@renderer/enum/format.enum';
 
 export class SystemBasicModel extends BaseModel {
-  public network: number = 10;
+  public network = 10;
+  public format: FormatEnum = FormatEnum.JSON;
 }

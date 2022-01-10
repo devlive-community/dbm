f09644b14ba0d71b9459064b8647503ac07094be
qianmoQ <shicheng@ttxit.com>
2022-01-10 18:18:52 +0800

    [Refactor] Supports configuration of application languages

--- src/renderer/app/layout/layout.component.ts
+++ src/renderer/app/layout/layout.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { BasicService } from '@renderer/services/system/basic.service';
+import { SystemBasicModel } from '@renderer/model/system.model';
 
 @Component({
   selector: 'app-layout',
@@ -10,7 +12,7 @@ import { TranslateService } from '@ngx-translate/core';
         margin: 16px 0;
       }
 
-      .ant-layout-header{
+      .ant-layout-header {
         padding: 0;
       }
 
@@ -34,8 +36,10 @@ import { TranslateService } from '@ngx-translate/core';
   ]
 })
 export class LayoutComponent implements OnInit {
-  constructor(private translate: TranslateService) {
-    this.translate.setDefaultLang('en');
+  constructor(private translate: TranslateService,
+              private basicService: BasicService) {
+    const basicConfig = this.basicService.get() === null ? new SystemBasicModel() : this.basicService.get();
+    this.translate.setDefaultLang(basicConfig.language);
   }
 
   ngOnInit() {

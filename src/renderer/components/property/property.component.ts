67fbb9961b363705df8e530b59567a2670c97889
qianmoQ <shicheng@ttxit.com>
2022-01-27 14:56:11 +0800

    [Enhancement] Optional parameters are supported

--- src/renderer/components/property/property.component.ts
+++ src/renderer/components/property/property.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { BaseComponent } from '@renderer/app/base.component';
 import { PropertyModel } from '@renderer/model/property.model';
 import { StringUtils } from '@renderer/utils/string.utils';
+import * as cloneDeep from 'lodash/cloneDeep';
 
 @Component({
   selector: 'app-component-property',
@@ -29,7 +30,9 @@ export class PropertyComponent extends BaseComponent {
       element.value = this.modelValues[index];
     });
     this.property.properties = this.properties;
-    const empty = Object.keys(this.property.properties).filter(item => StringUtils.isEmpty(this.property.properties[item].value));
+    const _properties = cloneDeep(this.property.properties);
+    const empty = Object.keys(_properties.filter(f => f.required))
+      .filter(item => StringUtils.isEmpty(this.property.properties[item].value));
     if (empty.length <= 0) {
       this.property.validate = true;
     } else {

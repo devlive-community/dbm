fd3324790c594b6ee71d9366e012d816cf30968c
qianmoQ <shicheng@ttxit.com>
2022-03-08 16:11:16 +0800

    [Enhancement] Support visual construction of data table and configuration of optional parameters

--- src/renderer/model/database.model.ts
+++ src/renderer/model/database.model.ts
@@ -4,6 +4,7 @@ import { PropertyModel } from '@renderer/model/property.model';
 
 export class DatabaseModel {
   name: string;
+  targetName: string; // input value
   description: string;
   type: DatabaseEnum;
   engines: DatabaseModel[];
@@ -12,6 +13,7 @@ export class DatabaseModel {
   table: string;
   icon: string;
   experimental: boolean;
+  validate: boolean; // Check whether dependent components pass the verification
   /**
    * Customize configuration parameters
    * You are advised to use the key specified in the internationalization file to customize configuration parameters and field description
@@ -22,6 +24,10 @@ export class DatabaseModel {
    * name: Convert to internal key --> value form, for example Engine HDFS(uri= XXX)
    */
   propertyType: PropertyEnum;
+  /**
+   * Optional parameter. The user can customize the processing based on whether the user enters a value
+   */
+  optionalProperties: PropertyModel[];
 
   public static builder(name: string,
                         description: string,

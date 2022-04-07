04b06da22386ea404135eda7bbb1139e47ecb566
qianmoQ <shicheng@ttxit.com>
2022-04-07 11:37:02 +0800

    [Enhancement] Query history supports IndexDB storage

--- src/renderer/services/persistence.service.ts
+++ src/renderer/services/persistence.service.ts
@@ -1,6 +1,4 @@
-import { BaseModel } from '@renderer/model/base.model';
-
 export abstract class PersistenceService {
-  public abstract save(model: BaseModel): any;
-  public abstract getAll(): BaseModel[];
+  public abstract save(model: any): any;
+  public abstract getAll(): any[];
 }

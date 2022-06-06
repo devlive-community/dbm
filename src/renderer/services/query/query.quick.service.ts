31eb8a04b5228ae5ee43ed3a510f3ab4b8a67cf6
qianmoQ <shicheng@ttxit.com>
2022-06-06 15:26:47 +0800

    Fix the exception of Presto using quick query

--- src/renderer/services/query/query.quick.service.ts
+++ src/renderer/services/query/query.quick.service.ts
@@ -2,16 +2,16 @@ export class QueryQuickService {
   getQuickAll() {
     return [{
       name: 'DESCRIBE ...',
-      value: 'DESCRIBE `{0}`.`{1}`'
+      value: 'DESCRIBE {0}.{1}'
     }, {
       name: 'SHOW CREATE TABLE ...',
-      value: 'SHOW CREATE TABLE `{0}`.`{1}`'
+      value: 'SHOW CREATE TABLE {0}.{1}'
     }, {
       name: 'SELECT ... LIMIT 100',
-      value: 'SELECT * FROM `{0}`.`{1}` LIMIT 100'
+      value: 'SELECT * FROM {0}.{1} LIMIT 100'
     }, {
       name: 'SELECT COUNT FROM ...',
-      value: 'SELECT COUNT(1) FROM `{0}`.`{1}`'
+      value: 'SELECT COUNT(1) FROM {0}.{1}'
     }, {
       name: 'SHOW PARTITIONS FROM ...',
       value: 'SELECT `partition` FROM `system`.parts WHERE `database` = \'{0}\' AND `table` = \'{1}\''

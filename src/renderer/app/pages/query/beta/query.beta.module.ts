441e54d6443d44ee60b2c1f729b6370369905a5f
qianmoQ <shicheng@ttxit.com>
2022-07-21 11:10:54 +0800

    The new query dynamically loads the service according to the plug-in

--- src/renderer/app/pages/query/beta/query.beta.module.ts
+++ src/renderer/app/pages/query/beta/query.beta.module.ts
@@ -1,23 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
-import { QueryBetaComponent } from './query.beta.component';
+import { CodemirrorModule } from "@ctrl/ngx-codemirror";
 import { TranslateModule } from '@ngx-translate/core';
-import { DatasourceService } from '@renderer/services/management/datasource.service';
-import { CommonModule } from '@angular/common';
-import { QueryService } from '@renderer/services/query/query.service';
-import { NgZorroAntdModule } from '@renderer/app/ng-zorro-antd.module';
 import { CommonShareModule } from '@renderer/app/common-share.module';
-import { DatabaseService } from "@renderer/services/management/database.service";
-import { TableService } from "@renderer/services/management/table.service";
-import { ColumnService } from "@renderer/services/management/column.service";
+import { NgZorroAntdModule } from '@renderer/app/ng-zorro-antd.module';
+import { PluginFactory } from '@renderer/factory/plugin.factory';
+import { ClickHousePlugin } from '@renderer/plugin/clickhouse.plugin';
 import { IconCommonService } from "@renderer/services/common/icon.common.service";
 import { MenuCommonService } from "@renderer/services/common/menu.common.service";
-import { CodemirrorModule } from "@ctrl/ngx-codemirror";
 import { EditorService } from "@renderer/services/editor/editor.service";
+import { ColumnService } from "@renderer/services/management/column.service";
+import { DatabaseService } from "@renderer/services/management/database.service";
+import { DatasourceService } from '@renderer/services/management/datasource.service';
+import { TableService } from "@renderer/services/management/table.service";
+import { QueryService } from '@renderer/services/query/query.service';
+import { PluginToken } from '@renderer/token/plugin.token';
 import { AceModule } from "ngx-ace-wrapper";
 import { TableModule } from "ngx-easy-table";
 import { EllipsisModule } from "ngx-ellipsis";
+import { QueryBetaComponent } from './query.beta.component';
 
 const QUERY_ROUTES: Routes = [
   {path: '', component: QueryBetaComponent}
@@ -49,7 +52,9 @@ const QUERY_ROUTES: Routes = [
     IconCommonService,
     MenuCommonService,
     EditorService,
-    QueryService
+    QueryService,
+    PluginFactory,
+    { provide: PluginToken, useClass: ClickHousePlugin, multi: true }
   ]
 })
 export class QueryBetaModule {
---
+++ src/renderer/token/plugin.token.ts
@@ -0,0 +1,4 @@
+import { InjectionToken } from '@angular/core';
+import { PluginInterface } from '@renderer/interfaces/plugin.interface';
+
+export const PluginToken = new InjectionToken<PluginInterface>('');

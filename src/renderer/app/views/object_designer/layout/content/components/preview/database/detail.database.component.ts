e887ca3e7ea2d741ae4a076720bcfd67ce1e76d5
qianmoQ <shicheng@ttxit.com>
2022-08-30 21:13:31 +0800

    Support delete table

--- src/renderer/app/views/object_designer/layout/content/components/preview/database/detail.database.component.ts
+++ src/renderer/app/views/object_designer/layout/content/components/preview/database/detail.database.component.ts
@@ -7,6 +7,7 @@ import { StringUtils } from "@renderer/utils/string.utils";
 import { ActionEnum } from "@renderer/enum/action.enum";
 import { TypeEnum } from "@renderer/enum/type.enum";
 import { AssertUtils } from "@renderer/app/views/object_designer/utils/assert.utils";
+import { NzMessageService } from "ng-zorro-antd/message";
 
 @Component({
   selector: 'object-designer-layout-content-detail-database',
@@ -17,43 +18,89 @@ export class LayoutContentDetailDatabaseComponent implements AfterViewInit {
   @Input()
   applyData: DesignerApplyData;
 
+  loading = {
+    tableContainer: false,
+  }
   tableDataSize = 0;
   applyResultColumns = [];
-  loading = {
-    tableContainer: false
+  applySelectTable = {
+    checkedTables: new Set<string>()
+  }
+  deleteTable = {
+    visible: false,
+    loading: false
   }
 
   constructor(private pluginFactory: PluginFactory,
-              private configFactory: ConfigFactory) {
+              private configFactory: ConfigFactory,
+              private messageService: NzMessageService) {
   }
 
   ngAfterViewInit(): void {
-    setTimeout(() => {
-      this.loading.tableContainer = true;
-      this.tableDataSize = 0;
-      const request = new RequestModel();
-      request.config = this.applyData.dataSource;
-      const type = this.applyData.dataSource.type;
-      const sql = StringUtils.format(this.configFactory.createConfig(type).getStatement('metadataManagementFetchTables'),
-        [this.applyData.database]);
+    setTimeout(() => this.handlerInitializer(), 0)
+  }
 
-      this.pluginFactory.createService(type)
-        .getResponse(request, sql)
-        .then(response => {
-          if (response.status) {
-            this.tableDataSize = response.data.rows;
-            if (this.tableDataSize > 0) {
-              this.applyResultColumns = response.data.columns;
-            }
+  handlerInitializer() {
+    this.loading.tableContainer = true;
+    this.tableDataSize = 0;
+    const request = new RequestModel();
+    request.config = this.applyData.dataSource;
+    const type = this.applyData.dataSource.type;
+    const sql = StringUtils.format(this.configFactory.createConfig(type).getStatement('metadataManagementFetchTables'),
+      [this.applyData.database]);
+
+    this.pluginFactory.createService(type)
+      .getResponse(request, sql)
+      .then(response => {
+        if (response.status) {
+          this.tableDataSize = response.data.rows;
+          if (this.tableDataSize > 0) {
+            this.applyResultColumns = response.data.columns;
           }
-          this.loading.tableContainer = false;
-        });
-    }, 0)
+        }
+        this.loading.tableContainer = false;
+      });
   }
 
   handlerNewTable() {
     this.applyData.command.action = ActionEnum.create;
     this.applyData.command.type = TypeEnum.table;
+  }
+
+  handlerTableChecked(tableName: string, checked: boolean) {
+    if (checked) {
+      this.applySelectTable.checkedTables.clear();
+      this.applySelectTable.checkedTables.add(tableName);
+      this.applyData.table = tableName;
+    } else {
+      this.applySelectTable.checkedTables.delete(tableName);
+      this.applyData.table = null;
+    }
+  }
+
+  handlerApplyDeleteTable() {
+    this.deleteTable.visible = true;
+  }
+
+  handlerCancelDeleteTable() {
+    this.deleteTable.visible = false;
+  }
+
+  handlerDeleteTable() {
+    this.deleteTable.loading = true;
+    const request = new RequestModel();
+    request.config = this.applyData.dataSource;
+    const sql = StringUtils.format('DROP TABLE {0}.{1}', [this.applyData.database, this.applyData.table]);
+    this.pluginFactory.createService(this.applyData.dataSource.type)
+      .getResponse(request, sql)
+      .then(response => {
+        if (response.status) {
+          this.messageService.success("OK");
+          this.handlerInitializer();
+        }
+        this.deleteTable.loading = false;
+        this.deleteTable.visible = false;
+      });
   }
 
   isCreateTable(): boolean {
